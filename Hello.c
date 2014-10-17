ca4867aa5117048f10123e8aac504bf473db5b04
wongxingjun <wongxingjun@126.com>
2014-10-17 17:39:43 +0800

    practice

---
+++ Hello.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+__attribute((constructor)) void before_main()  
+{
+   	printf("%s\n",__FUNCTION__);  
+}  
+  
+__attribute((destructor)) void after_main()  
+{  
+   	printf("%s\n",__FUNCTION__);  
+}  
+  
+int main( int argc, char ** argv )  
+{  
+   	printf("%s\n",__FUNCTION__);  
+	return 0;  
+}  
+
