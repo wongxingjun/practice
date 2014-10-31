1ed2b794a3a7c9cbf7804e32b1b236fec82f7bf2
wongxingjun <wongxingjun@126.com>
2014-10-31 19:19:34 +0800

    update

--- StreamingWordCount/src/main/scala/sparkstreaming/example/HdfsWordCount.scala
+++ StreamingWordCount/src/main/scala/sparkstreaming/example/HdfsWordCount.scala
@@ -1,37 +1,9 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package sparkstreaming.example
 
 import org.apache.spark.SparkConf
 import org.apache.spark.streaming.{Seconds, StreamingContext}
 import org.apache.spark.streaming.StreamingContext._
 
-/**
- * Counts words in new text files created in the given directory
- * Usage: HdfsWordCount <directory>
- *   <directory> is the directory that Spark Streaming will use to find and read new text files.
- *
- * To run this on your local machine on directory `localdir`, run this example
- *    $ bin/run-example \
- *       org.apache.spark.examples.streaming.HdfsWordCount localdir
- *
- * Then create a text file in `localdir` and the words in the file will get counted.
- */
 object HdfsWordCount {
   def main(args: Array[String]) {
     if (args.length < 1) {
@@ -51,6 +23,6 @@ object HdfsWordCount {
     val wordCounts = words.map(x => (x, 1)).reduceByKey(_ + _)
     wordCounts.print()
     ssc.start()
-    ssc.stop(true,true)
+    ssc.stop(true,true) //stop the stream after processing
   }
 }
